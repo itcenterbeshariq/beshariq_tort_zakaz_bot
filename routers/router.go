f408af636636fcc2e1e784e600fc476b6fc6d304
Abu <abdulloxmutalimov@gmail.com>
2025-09-03 15:11:50 +0500

    index html

--- routers/router.go
+++ routers/router.go
@@ -6,5 +6,6 @@ import (
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/login", &controllers.AuthController{}, "get:LoginForm;post:Login")
 }
